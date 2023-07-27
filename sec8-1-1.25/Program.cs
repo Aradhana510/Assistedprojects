d69359d9753dd8b9eefa92971cf39a19b08e8469
Aradhana singh <66427825+Aradhana510@users.noreply.github.com>
2023-07-26 23:10:15 -0700

    Add files via upload

---
+++ sec8-1-1.25/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RecursionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        static void runApp()
+        {
+            string[] students = { "Alice", "Bob", "Charlie", "David", "Eva" };
+            int currentIndex = 0;
+            printViaRecursion(students, currentIndex);
+        }
+
+        static void printViaRecursion(string[] arr, int index)
+        {
+            // Base case: If the index goes out of bounds, return and stop the recursion.
+            if (index >= arr.Length)
+                return;
+
+            // Print the name at the current index
+            Console.WriteLine(arr[index]);
+
+            // Call the method recursively with the next index
+            printViaRecursion(arr, index + 1);
+        }
+    }
+}
