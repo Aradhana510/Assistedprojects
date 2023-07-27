3b5f8a2f374d5c590721faf462ee336ac829cebe
Aradhana singh <66427825+Aradhana510@users.noreply.github.com>
2023-07-26 23:22:06 -0700

    Add files via upload

---
+++ sec8-1-1.31/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace QuickSortDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        static void runApp()
+        {
+            // Sample marks of subjects
+            int[] marks = { 75, 80, 60, 90, 70, 85, 95, 50, 65, 55 };
+
+            Console.WriteLine("Unsorted Marks:");
+            PrintArray(marks);
+
+            QuickSort(marks, 0, marks.Length - 1);
+
+            Console.WriteLine("\nSorted Marks in Ascending Order:");
+            PrintArray(marks);
+        }
+
+        static void QuickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int partitionIndex = Partition(arr, low, high);
+
+                QuickSort(arr, low, partitionIndex - 1);
+                QuickSort(arr, partitionIndex + 1, high);
+            }
+        }
+
+        static int Partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+
+            Swap(arr, i + 1, high);
+            return i + 1;
+        }
+
+        static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
